e688fc1dd220889099aa389d85bee1f54705eb3c
Fabrício <jfmsantos@outlook.com>
2022-08-22 22:58:12 -0300

    Zuul timeout

--- hr-worker/src/main/java/com/devsuperior/hrworker/recourses/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/recourses/WorkerResource.java
@@ -37,10 +37,10 @@ public class WorkerResource {
 	@GetMapping (value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		
-		/*
-		 * try { Thread.sleep(3000L); } catch (InterruptedException e) {
-		 * e.printStackTrace(); }
-		 */
+		
+		  try { Thread.sleep(3000L); } catch (InterruptedException e) {
+		  e.printStackTrace(); }
+		 
 		
 		logger.info("Port = " + env.getProperty("local.server.port"));
 		
