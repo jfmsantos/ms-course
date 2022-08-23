2340c5c068dc96b1dc76434dd68be016c504d5ec
Fabrício <jfmsantos@outlook.com>
2022-08-23 00:17:09 -0300

    hr-worker as config server client

--- hr-worker/src/main/java/com/devsuperior/hrworker/recourses/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/recourses/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,37 +17,44 @@ import com.devsuperior.hrworker.entities.Worker;
 import com.devsuperior.hrworker.repositories.WorkerRepository;
 
 @RestController
-@RequestMapping (value = "/workers")
+@RequestMapping(value = "/workers")
 public class WorkerResource {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(WorkerRepository.class);
-	
+
+	@Value("${test.config}")
+	private String testCongif;
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private WorkerRepository repository;
-	
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testCongif);
+		return ResponseEntity.noContent().build();
+	}
+
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = repository.findAll();
 		return ResponseEntity.ok(list);
-		
+
 	}
-	
-	@GetMapping (value = "/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		
-		
-		  try { Thread.sleep(3000L); } catch (InterruptedException e) {
-		  e.printStackTrace(); }
-		 
-		
+
+//		  try { Thread.sleep(3000L); } catch (InterruptedException e) {
+//		  e.printStackTrace(); }
+
 		logger.info("Port = " + env.getProperty("local.server.port"));
-		
+
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
-		
+
 	}
 
 }
